cd806ee323fb3dbc368179d2d9572610f23c7b77
Mario Paucar <ingmariopaucar@gmail.com>
2024-12-29 10:39:29 -0500

    alumnos

--- Dia_4/03_crud_alumnos.py
+++ Dia_4/03_crud_alumnos.py
@@ -1,39 +1,34 @@
 import os
 from time import sleep
 from matricula.lib_alumnos import *
-    
+
 """
-    CRUD
-    - CREATE
-    - READ
-    - UPDATE
-    - DELETE
-
-"""  
+CRUD
+ - CREATE
+ - READ
+ - UPDATE
+ - DELETE
+"""
 cargar_alumnos('alumnos.txt')
-opcion=0
+opcion = 0
 
-    
-while(opcion<5):
-    os.system("clear")   
+while(opcion < 5):
+    os.system("clear")
     menu()
-    opcion =int(input("INGRESE OPCIÓN : "))
-    os.system("clear")     
-   
-    if opcion ==1:
-        dic_nuevo_alumno=registrar()
-        dic_alumnos.update(dic_nuevo_alumno) 
-    elif opcion ==2:
+    opcion = int(input("INGRESE OPCION : "))
+    os.system("clear")
+    if opcion == 1:
+        registrar()
+    elif opcion == 2:
         mostrar()
-        input("Presiona ENTER para continuar ...")
-    elif opcion ==3:
-        actualizar() 
-    elif opcion ==4:
+        input("Presion ENTER para continuar...")
+    elif opcion == 3:
+        actualizar()
+    elif opcion == 4:
         eliminar()
-    elif opcion ==5:
+    elif opcion == 5:
         grabar_alumnos('alumnos.txt')
         mostrar_mensaje("[5] SALIR")
     else:
-        mostrar_mensaje("OPCIÓN INVALIDA!!!")
-                    
+        mostrar_mensaje("OPCIÓN INVÁLIDA!!!")
     sleep(1)
--- Dia_4/matricula/lib_alumnos.py
+++ Dia_4/matricula/lib_alumnos.py
@@ -1,107 +1,99 @@
+ANCHO = 20
 
-ANCHO=20
-
-dic_alumnos={ }
+dic_alumnos = {}
 
 def cargar_alumnos(file_name):
-    file=open(file_name,'r')
-    str_alumnos=file.read()
+    file = open(file_name,'r')
+    str_alumnos = file.read()
     file.close()
-    lista_alumnos=str_alumnos.splitlines()
+    lista_alumnos = str_alumnos.splitlines()
     for str_fila in lista_alumnos:
-        lista_fila=str_fila.split(',')
-        dic_fila={
+        lista_fila = str_fila.split(',')
+        dic_fila = {
             'nombre':lista_fila[1],
             'email':lista_fila[2]
         }
-        dic_nuevo_alumno={
-            lista_fila[0]:dic_fila
+        dic_nuevo_alumno = {
+            lista_fila[0]: dic_fila
         }
         dic_alumnos.update(dic_nuevo_alumno)
 
 def grabar_alumnos(file_name):
-    str_alumnos=""
+    str_alumnos = ""
     for alumno_clave,alumno_valor in dic_alumnos.items():
-        str_alumnos+=alumno_clave
+        str_alumnos += alumno_clave
+        str_alumnos += ','
         for registro_clave,registro_valor in alumno_valor.items():
-            str_alumnos+=registro_valor
-            if registro_clave !='email':
-                str_alumnos+=','
+            str_alumnos += registro_valor
+            if registro_clave != 'email':
+                str_alumnos += ','
             else:
-                str_alumnos+='\n'
-                
-    fsalida=open(file_name,'w')
+                str_alumnos += '\n'
+    
+    fsalida = open(file_name,'w')
     fsalida.write(str_alumnos)
     fsalida.close()
-
-
+    
 
 def mostrar_mensaje(texto):
-    print("*"*ANCHO+"*"*ANCHO)
+    print("*" * ANCHO + "*" * ANCHO)
     if texto != " ":
-        print(" "*10 + texto)
-        print("*"*ANCHO+"*"*ANCHO)
+        print(" " * 10 + texto)
+        print("*" * ANCHO + "*" * ANCHO)
 
-
-#función menu que se usa en crud_alumnos
 def menu():
-    mostrar_mensaje("GESTION DE ALUMNOS")
+    mostrar_mensaje("GESTIÓN DE ALUMNOS")
     print("""
          [1] REGISTRAR ALUMNO
          [2] MOSTRAR ALUMNOS
          [3] ACTUALIZAR ALUMNO
          [4] ELIMINAR ALUMNO
          [5] SALIR
-              """)
-    mostrar_mensaje("")
-    
-#función que registra nuevos alumnos
-def registrar ():
-    mostrar_mensaje("[1] INGRSAR ALUMNO")
-    dni = input("DNI   :")
-    nombre = input("NOMBRE    :")
-    email =input("EMAIL    :")
-    dic_nuevo_alumno ={
-        dni :{
-            'nombre':nombre,
-            'email':email
-            }
-    }
-    return dic_nuevo_alumno
+          """)
+    mostrar_mensaje(" ")
 
-#función que muestra los alumnos registrados
+def registrar():
+    mostrar_mensaje("[1] REGISTRAR ALUMNO")
+    dni = input("DNI    :")
+    nombre = input("NOMBRE  :")
+    email = input("EMAIL    :")
+    dic_nuevo_alumno = {
+        dni : {
+                'nombre':nombre,
+                'email': email
+                }
+    }
+    dic_alumnos.update(dic_nuevo_alumno)
+
 def mostrar():
     mostrar_mensaje("[2] MOSTRAR ALUMNOS")
     for dni,datos in dic_alumnos.items():
-        print(f"DNI:{'dni'}")
-        print(f"Nombre:{datos['nombre']}")
-        print(f"EMAIL:{datos['email']}")
-        mostrar_mensaje("") 
-        
+        print(f"DNI : {dni}")
+        print(f"Nombre : {datos['nombre']}")
+        print(f"EMAIL : {datos['email']}")
+        mostrar_mensaje(" ")
 
 def actualizar():
     mostrar_mensaje("[3] ACTUALIZAR ALUMNO")
-    dni=input("INGRESE DNI DEL ALUMNO A ACTUALIZAR :")
+    dni = input("INGRESE DNI DEL ALUMNO A ACTUALIZAR")
     if dni in dic_alumnos:
-        print(f"ALUMNO A ACTUALIZAR DNI {dic_alumnos[dni]['nombre']}")
-        nuevo_nombre=input('NOMBRE:')
-        nuevo_email=input('EMAIL:')
-        dic_act_alumno={
+        print(f"ALUMNO A ACTUALIZAR  {dic_alumnos[dni]['nombre']}")
+        nuevo_nombre = input('NOMBRE : ')
+        nuevo_email = input('EMAIL :')
+        dic_act_alumno = {
             dni : {
                 'nombre':nuevo_nombre,
-                'email': nuevo_email
-                }
+                'email':nuevo_email
             }
-    dic_alumnos.update(dic_act_alumno)
-    print("ALUMNO ACTUALIZADO CON EXITO")
-    
-def eliminar ():
+        }
+        dic_alumnos.update(dic_act_alumno)
+        print("ALUMNO ACTUALIZADO CON EXITO")
+
+def eliminar():
     mostrar_mensaje("[4] ELIMINAR ALUMNO")
-    dni=input("INGRESE EL DNI DEL ALUMNO A ELIMINAR :")
+    dni = input("INGRES EL DNI DEL ALUMNO A ELIMINAR : ")
     if dni in dic_alumnos:
         dic_alumnos.pop(dni)
         print("ALUMNO ELIMINADO")
     else:
         print("NO SE ENCONTRO EL ALUMNO")
-        
-
